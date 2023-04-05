a47b94c8b81928c7a02b8003c25a7421cc2cdc3c
Mr-Psmith <psancthus@gmail.com>
2023-04-05 16:08:25 +0000

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  /* const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+  if (storedUserLoggedInInfo === "1") {/* So if the above info shws us that the user already logged in, than we skip the login page 
+    setIsLoggedIn(true);  /* But this creates an infinite loop, as every time we call a state settinmg function this comp reexecutes
+  } */
+  /* But we can do it like this: */
+  useEffect(() => {  /* This will rerun only when the dependancies[] change  */
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInfo === "1") {
+      setIsLoggedIn(true); 
+    }
+  }, []);/* and bec this has none, it will run only at the start of the app */
+
+ 
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1") /* First is an identifgier for the stuf itself, second is an identifier for the status of the stuff */
+    setIsLoggedIn(true);                      /* We can do this in the function, bec, only executes when user clicks the given button */
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn"); /* and this is how we reset the logged in status */
     setIsLoggedIn(false);
   };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+
+const emailReducer = () => { /* A function in the useReducer for reducerFn */
+  
+}; 
 
 const Login = (props) => {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -11,20 +15,35 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer();
+
+  useEffect(() => {
+    const identifier = setTimeout(() => { /* with this we only check after 500ms */
+      setFormIsValid( 
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier); /* Sooooo this cleans the timeout after every run. And solves that the useEffect function settimeout part doesnt run in the background only when you stop typing for 500ms lecture 113. */
+    };
+  }, [enteredEmail, enteredPassword]);/* If we dont add depend. here, than it only runs once hence cant login. If we take out dependencies, than it runs every time, hence loop. So we add as dependencies what we are using az out sideeffect function */
+                                                          /* And like this it reruns if the given stuff changes */
+      /* There was also setFormIsValid, but we could omit that, bec those state updating functions by default are insured by React to never change. */
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
+    /* setFormIsValid( ORIGINALe
       event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
-  };
+    );*/
+  }; 
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
+    /* setFormIsValid(
       event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    ); */
   };
 
   const validateEmailHandler = () => {
