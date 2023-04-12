2332be85d10ed45b2e3ec2b606171794b7ad9586
Mr-Psmith <psancthus@gmail.com>
2023-04-12 16:33:25 +0000

    More context API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import AuthContext from './context/auth-context';
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from "./context/auth-context";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   /* const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
   if (storedUserLoggedInInfo === "1") {/* So if the above info shws us that the user already logged in, than we skip the login page 
     setIsLoggedIn(true);  /* But this creates an infinite loop, as every time we call a state settinmg function this comp reexecutes
   } */
   /* But we can do it like this: */
-  useEffect(() => {  /* This will rerun only when the dependancies[] change  */
+  useEffect(() => {
+    /* This will rerun only when the dependancies[] change  */
     const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
     if (storedUserLoggedInInfo === "1") {
-      setIsLoggedIn(true); 
+      setIsLoggedIn(true);
     }
-  }, []);/* and bec this has none, it will run only at the start of the app */
-
- 
+  }, []); /* and bec this has none, it will run only at the start of the app */
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1") /* First is an identifgier for the stuf itself, second is an identifier for the status of the stuff */
-    setIsLoggedIn(true);                      /* We can do this in the function, bec, only executes when user clicks the given button */
+    localStorage.setItem("isLoggedIn","1"); /* First is an identifgier for the stuf itself, second is an identifier for the status of the stuff */
+    setIsLoggedIn(
+      true
+    ); /* We can do this in the function, bec, only executes when user clicks the given button */
   };
 
   const logoutHandler = () => {
@@ -36,12 +36,14 @@ function App() {
 
   return (
     <React.Fragment>
-      <AuthContext.Provider value={{isLoggedIn: isLoggedIn, }}> {/* but we will be able to change this obj, throug state or else, and this changes will be passed down */}
-      <MainHeader onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider /* but we will be able to change this obj, throug state or else, and this changes will be passed down */
+        value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }} /* "And just like this" we can pass onLogout here as well. Dunno why, didnt say it */
+      >
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
       </AuthContext.Provider>
     </React.Fragment>
   );
--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Login from "../components/Login/Login";
 
 const AuthContext = React.createContext({ /* We get back an object that also contains components */
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 }); /* Creates a context objectm, or anything else */
 
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        setIsLoggedIn(true);
+    };
+
+    return <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }} >{props.children}</AuthContext.Provider>;
+};
 
 export default AuthContext;
